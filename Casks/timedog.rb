d3a19c4276ad581e95d166e0e67935275201ba57
atomiyama <tomisuker16@gmail.com>
2026-03-07 00:31:16 +0900

    timedog: bump to v1.1.2

--- Casks/timedog.rb
+++ Casks/timedog.rb
@@ -1,6 +1,6 @@
 cask "timedog" do
-  version "1.1.1"
-  sha256 "ba233206a7b2c3610880aac4e9be9ce087da93f5445def6ceb4d57ecd967e683"
+  version "1.1.2"
+  sha256 "76e558bf5a0973684d6c203ca48b4b7a3fb6805229ebd0f0ce7ba12eb4827e4e"
 
   url "https://github.com/a-tommyyy/timedog/releases/download/v#{version}/Timedog-#{version}-arm64.dmg"
   name "Timedog"
